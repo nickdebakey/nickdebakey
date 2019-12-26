a28d17561222de2fe8ca2d1654463f6a5ccc735a
ndebakey <ndebakey@uncc.edu>
2019-12-26 16:41:41 -0500

    Adjusted rainbow css borders as well as better optimized JavaScript notation.

--- about.js
+++ about.js
@@ -1,9 +1,13 @@
-/* Set the width of the sidebar to 250px (show it) */
+var $ = function(id) {
+    return document.getElementById(id);
+};
+
+/* set the width of the sidebar to 100% (show it) */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "100%";
+    $("mySidebar").style.width = "100%";
 }
 
-/* Set the width of the sidebar to 0 (hide it) */
+/* set the width of the sidebar to 0% (hide it) */
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
+    $("mySidebar").style.width = "0";
 }
