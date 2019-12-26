a7c068b6dc7283f7a2f0ad331c762ad0162d2d45
ndebakey <ndebakey@uncc.edu>
2019-12-26 16:15:31 -0500

    Reconstructed website from commit so it was designed in "mobile-first" form. Removed lots of elements as well as centered "main" body in html, added a sidebar navigation rather than having a navigation bar.

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,11 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100%";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
+
 var lightbox = $('.gallery a').simpleLightbox(options);
